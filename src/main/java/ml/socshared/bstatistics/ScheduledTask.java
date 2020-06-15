884e1b1a432ed08377dc171d4a437f4f920a2960
ageev-aleksey <ageevav97@gmail.com>
2020-06-15 21:47:00 +0300

    delay 28.8 minutes

--- src/main/java/ml/socshared/bstatistics/ScheduledTask.java
+++ src/main/java/ml/socshared/bstatistics/ScheduledTask.java
@@ -54,7 +54,7 @@ public class ScheduledTask {
     private final  SentrySender sentrySender;
     private final  RabbitTemplate rabbitTemplate;
 
-    private  final int delay =60000; //1728000;//28,8 minutes
+    private  final int delay =1728000; //1728000;//28,8 minutes
     private  final int milli = 1000;
 
     LocalDateTime beforeDelayStart = LocalDateTime.now().minusMinutes(delay/milli);
